42b8885d074b9d2f7c81212567a43e74e7d149c3
herman <phm0403@126.com>
2019-12-17 16:46:59 +0800

    词向量质量测试

---
+++ TestW2V.py
@@ -0,0 +1,25 @@
+import sys
+from gensim.models import Word2Vec
+import logging
+import os
+
+program_name, model_name, test_word, test_linear = sys.argv[:4]
+## logger设计
+program = os.path.basename(program_name)
+logger = logging.getLogger(program)
+logging.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s')
+logging.root.setLevel(level=logging.INFO)
+####
+
+logger.info("running %s" % ' '.join(sys.argv))
+
+wiki_model = Word2Vec.load(model_name)
+
+logger.info(wiki_model.wv.most_similar(test_word))
+
+should_test_linear = bool(test_linear)
+
+if should_test_linear:
+    x1, x2, y1 = sys.argv[4:7]
+    result = wiki_model.most_similar(positive=[y1, x2], negative=[x1])
+    logger.info(result)
