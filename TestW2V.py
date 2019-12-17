b1482f5e35a300e6909add0a9dfb85d601bd5c8b
herman <phm0403@126.com>
2019-12-17 18:06:12 +0800

    bool函数有个bug：除了空字符串外，其它结果都是True

--- TestW2V.py
+++ TestW2V.py
@@ -3,7 +3,7 @@ from gensim.models import Word2Vec
 import logging
 import os
 
-program_name, model_name, test_word, test_linear = sys.argv[:4]
+program_name, model_name, test_word, should_test_linear = sys.argv[:4]
 ## logger设计
 program = os.path.basename(program_name)
 logger = logging.getLogger(program)
@@ -17,9 +17,7 @@ wiki_model = Word2Vec.load(model_name)
 
 logger.info(wiki_model.wv.most_similar(test_word))
 
-should_test_linear = bool(test_linear)
-
-if should_test_linear:
+if int(should_test_linear):
     x1, x2, y1 = sys.argv[4:7]
     result = wiki_model.most_similar(positive=[y1, x2], negative=[x1])
     logger.info(result)
